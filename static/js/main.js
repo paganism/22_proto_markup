1005a27baa220d88e4112500ba43779ee3464684
yuriy_beloff <paganismrus@gmail.com>
2018-06-07 00:14:15 +0300

    deleted tabs

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ $.getJSON('https://api.sypexgeo.net').done(function (location){
        {
            icon: 'glyphicon glyphicon-globe',
            title: city + ', ' + country,
-           message: 'Правильно,'
+           message: 'Правильно'
        },
        {
            type: 'info',
@@ -20,15 +20,15 @@ $.getJSON('https://api.sypexgeo.net').done(function (location){
            delay: 3000,
            icon_type: 'class',
            template:   '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
-								  '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
-								  '<span data-notify="icon"></span> ' +
-								  '<span data-notify="title">Ваш регион: <b>{1}</b></span> ' +
-								  '<div class="text-center"><span data-notify="message">{2}</span></div>' +
-								  '<div class="text-center">'+
-							    '<a class="btn btn-info btn-sm" data-notify="dismiss">Да</a>' +
-							    '<span>&nbsp;</span>' +
-							    '<a class="btn btn-info btn-sm" data-notify="dismiss" id="button_error_region">Нет</a>' +
-								  '</div>'+
+                  '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                  '<span data-notify="icon"></span> ' +
+                  '<span data-notify="title">Ваш регион: <b>{1}</b></span> ' +
+                  '<div class="text-center"><span data-notify="message">{2}</span></div>' +
+                  '<div class="text-center">'+
+                  '<a class="btn btn-info btn-sm" data-notify="dismiss">Да</a>' +
+                  '<span>&nbsp;</span>' +
+                  '<a class="btn btn-info btn-sm" data-notify="dismiss" id="button_error_location">Нет</a>' +
+                  '</div>'+
                                 '</div>'
        }
    )
@@ -38,6 +38,6 @@ $('.carousel').carousel({interval: 3000});
 
 var regionList = ['Московская область', 'Ленинградская область', 'Новгородская область', 'Псковская область', 'Мурманская область'];
 $.each(regionList, function(i){
-	var li = '<li><a href=#>' + regionList[i] + '</a></li>';
-	$('ul.dropdown-menu.regions').append(li)
+  var li = '<li><a href=#>' + regionList[i] + '</a></li>';
+  $('ul.dropdown-menu.regions').append(li)
 })
